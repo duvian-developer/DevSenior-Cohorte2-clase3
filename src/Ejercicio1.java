cfc797fac6eacfc4e2d466a820f556f16901396e
Duvian Restrepo <dubianrestrepo@gmail.com>
2025-01-19 10:18:24 -0500

    Ejercicios propuestos clase 02

---
+++ src/Ejercicio1.java
@@ -0,0 +1,8 @@
+public class Ejercicio1{
+    public static void main(String[] args) {
+
+        var division = 6/2*(1+2);
+        System.out.println(division);
+        
+    }
+}
---
+++ src/Ejercicio2.java
@@ -0,0 +1,15 @@
+public class Ejercicio2 {
+    public static void main(String[] args) {
+
+        var precio_100_Gramos = 5.95;
+        var precioKilo = 29.75;
+        var valorRealPrecioKilo = precio_100_Gramos*10;
+
+        System.out.println("El precio real por kilo es: " + valorRealPrecioKilo);
+
+        System.out.println("hay un error en el precio del kilo "+ precioKilo + " el valor deberia ser "+valorRealPrecioKilo);
+        
+       
+        
+    }
+}
