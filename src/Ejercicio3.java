eaf34ba0a34c310121d0f04801e4968b4bf0c017
Duvian Restrepo <dubianrestrepo@gmail.com>
2025-01-19 11:25:21 -0500

    solucion ejercicio 3 de la clase 02

--- src/Ejercicio3.java
+++ src/Ejercicio3.java
@@ -1,5 +1,10 @@
 public class Ejercicio3 {
     public static void main(String[] args) {
+
+        var pecesRojos=284;
+        var pecesAzules=163;
+        var totalPecesAcuario = pecesRojos + pecesAzules;
+        System.out.println("La cantidad de peces en el acuario es de: " + totalPecesAcuario);
         
     }
 }
