26cabe8531d2c4dc6e8e5b43fa94794d1c379f06
Duvian Restrepo <dubianrestrepo@gmail.com>
2025-01-19 12:23:59 -0500

    Solución ejercicio 4 y 5

--- src/Ejercicio4.java
+++ src/Ejercicio4.java
@@ -1,5 +1,11 @@
 public class Ejercicio4 {
     public static void main(String[] args) {
+
+        var dineroInicialCarmen = 23;
+        var dineroFinalCarmen = 12.75;
+        var gastoTotalCarmen = 23 - 12.75;
+
+        System.out.println("Carmen se ha gastado: " + gastoTotalCarmen);
         
     }
 }
--- src/Ejercicio5.java
+++ src/Ejercicio5.java
@@ -1,5 +1,13 @@
 public class Ejercicio5 {
     public static void main(String[] args) {
+
+        var descuentoPC= 0.10;
+        var costoPC = 660;
+        var valorDescuento = costoPC*descuentoPC;
+        var costoPCconDescuento = costoPC - valorDescuento;
+
+        System.out.println("El PC tiene un valor de: " + costoPC + " al aplicar un decuento del "+descuentoPC+"%"+ " El costo total del Pc sería de: "+costoPCconDescuento);
+
         
     }
 }
